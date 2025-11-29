99d186798584ff88d42f49b56866ca16730ff32d
Manny262 <manelin3108@gmail.com>
2025-11-29 15:26:06 +0100

    fortsett med auth.views

--- DjangoParentSupportDesk/DjangoSupportDesk/AuthApp/views.py
+++ DjangoParentSupportDesk/DjangoSupportDesk/AuthApp/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def scrLogin(request):
     return render(request, 'scrLogin.html')
 def login(request):
-    print("hei")
+    if request.method == 'POST':
+        Username = request.POST['username']
+        Password = request.POST['passsword']
+        
+        user = authenticate(username=Username, password=Password)
+        if user is not None:
+            login(request, user)
+            return redirect('scrCaseManagerMain')
+    
 
 
 def scrRegister(request):
