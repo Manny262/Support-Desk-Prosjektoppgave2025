14e047df58b3c45b175375cace0251212ca1152c
Manny262 <manelin3108@gmail.com>
2025-12-01 08:48:33 +0100

    Authentisering (login og register) er laget, men ikke testet. Må lage en funksjon eller middelvare for Staff siden. Opprettet middelware.py i Auth

--- DjangoParentSupportDesk/DjangoSupportDesk/AuthApp/views.py
+++ DjangoParentSupportDesk/DjangoSupportDesk/AuthApp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 
+
 # Create your views here.
 def scrLogin(request):
     return render(request, 'scrLogin.html')
@@ -16,14 +17,25 @@ def login(request):
         if user is not None:
             login(request, user)
             return redirect('scrCaseManagerMain')
-    
+        else:
+            messages.error(request, 'Feil brukernavn eller passord')
+            return render(request, 'scrLogin')
 
 
 def scrRegister(request):
     return render(request, 'scrRegister.html')
 
 def register(request):
-    print('hei')
+    if request.method == 'Post':
+        email = request.POST['email']
+        username = request.POST['username']
+        password1  = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            user = User.objects.create_user(email=email, username=username, password=password1)
+            user.save()
+            print('User created')
+            return redirect('scrLogin')
 
 def LogoutPage(request):
     return render(request, 'scrLogoutPage.html')
