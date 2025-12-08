5d55d832411061b570c499109d639e1c20369a37
Manny262 <manelin3108@gmail.com>
2025-12-08 19:14:00 +0100

    lagt til css for auth

--- DjangoParentSupportDesk/DjangoSupportDesk/DjangoProjectSupportDesk/urls.py
+++ DjangoParentSupportDesk/DjangoSupportDesk/DjangoProjectSupportDesk/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('AuthApp.urls')),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('', include('SharedApp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / 'AuthApp' / 'static')
